359343cd6f1829a70fbdc12d364672c23303d733
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-05-05 15:09:20 +0700

    Tạo dự án

---
+++ Falculty_NTU_GraduationProject/src/main/java/ntu/granduationproject/ntu/FalcultyNtuGraduationProjectApplication.java
@@ -0,0 +1,13 @@
+package ntu.granduationproject.ntu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalcultyNtuGraduationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalcultyNtuGraduationProjectApplication.class, args);
+	}
+
+}
---
+++ Falculty_NTU_GraduationProject/src/test/java/ntu/granduationproject/ntu/FalcultyNtuGraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.granduationproject.ntu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalcultyNtuGraduationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
