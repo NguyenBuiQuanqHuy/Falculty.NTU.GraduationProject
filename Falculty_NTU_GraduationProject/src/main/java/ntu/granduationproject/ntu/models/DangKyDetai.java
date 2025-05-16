aa42cf2173131de5fda8fe5cc9261f6623b667e9
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-05-16 23:06:18 +0700

    Create DangKyDetai.java

---
+++ Falculty_NTU_GraduationProject/src/main/java/ntu/granduationproject/ntu/models/DangKyDetai.java
@@ -0,0 +1,63 @@
+package ntu.granduationproject.ntu.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "dangkydetai")
+public class DangKyDetai {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name = "msdt",referencedColumnName = "msdt")
+	private Project msdt;
+	
+	@OneToOne
+	@JoinColumn(name = "mssv",referencedColumnName = "mssv")
+	private SinhVien mssv;
+	
+	private String trangthai;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Project getMsdt() {
+		return msdt;
+	}
+
+	public void setMsdt(Project msdt) {
+		this.msdt = msdt;
+	}
+
+	public SinhVien getMssv() {
+		return mssv;
+	}
+
+	public void setMssv(SinhVien mssv) {
+		this.mssv = mssv;
+	}
+
+	public String getTrangthai() {
+		return trangthai;
+	}
+
+	public void setTrangthai(String trangthai) {
+		this.trangthai = trangthai;
+	}
+	
+	
+}
