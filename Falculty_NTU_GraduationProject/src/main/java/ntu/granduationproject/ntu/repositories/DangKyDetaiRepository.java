b361a1b3c945ff86541328b23e29a94a1e11716e
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-05-21 20:19:54 +0700

    Update DangKyDetaiRepository.java

--- Falculty_NTU_GraduationProject/src/main/java/ntu/granduationproject/ntu/repositories/DangKyDetaiRepository.java
+++ Falculty_NTU_GraduationProject/src/main/java/ntu/granduationproject/ntu/repositories/DangKyDetaiRepository.java
@@ -3,13 +3,19 @@ package ntu.granduationproject.ntu.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ntu.granduationproject.ntu.models.DangKyDetai;
-import ntu.granduationproject.ntu.models.Project;
 
-@Repository
-public interface DangKyDetaiRepository extends JpaRepository<DangKyDetai, Integer> {
+public interface DangKyDeTaiRepository extends JpaRepository<DangKyDetai, Integer>{
 
-    List<DangKyDetai> findByMsdt(Project msdt);
+	List<DangKyDetai> findByMsdt_Msdt(int msdt);
+	DangKyDetai findByMsdt_MsdtAndMssv_Mssv(int msdt, String mssv);
+	int countByMsdt_MsdtAndTrangthai(int msdt, String trangthai);
+	@Query("SELECT COUNT(d) FROM DangKyDetai d WHERE d.msdt.msgv.msgv = :msgv AND d.msdt.theLoai.matheloai = :matheloai AND d.trangthai = 'Đã duyệt'")
+	int countByGiangVienAndTheLoai(@Param("msgv") String msgv, @Param("matheloai") int matheloai);
+	List<DangKyDetai> findByMsdt_MsdtAndTrangthai(int msdt, String trangthai);
+	List<DangKyDetai> findByMsdt_Msgv_MsgvAndTrangthaiAndMsdt_TheLoai_Matheloai(String msgv, String trangthai, int matheloai);
+	
 }
