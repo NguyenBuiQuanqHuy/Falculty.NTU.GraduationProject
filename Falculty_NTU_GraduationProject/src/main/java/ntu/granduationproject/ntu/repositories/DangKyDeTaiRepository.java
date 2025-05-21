750f1a6553c7d8dcec1d3ebcf8e7dd4e3a95bae9
TruongVoXuan <truong.75.cvan@gmail.com>
2025-05-21 22:24:59 +0700

    hoàn thành duyệt khoa

--- Falculty_NTU_GraduationProject/src/main/java/ntu/granduationproject/ntu/repositories/DangKyDeTaiRepository.java
+++ Falculty_NTU_GraduationProject/src/main/java/ntu/granduationproject/ntu/repositories/DangKyDeTaiRepository.java
@@ -8,14 +8,15 @@ import org.springframework.data.repository.query.Param;
 
 import ntu.granduationproject.ntu.models.DangKyDetai;
 
-public interface DangKyDeTaiRepository extends JpaRepository<DangKyDetai, Integer>{
+public interface DangKyDeTaiRepository extends JpaRepository<DangKyDetai, Integer> {
 
-	List<DangKyDetai> findByMsdt_Msdt(int msdt);
-	DangKyDetai findByMsdt_MsdtAndMssv_Mssv(int msdt, String mssv);
-	int countByMsdt_MsdtAndTrangthai(int msdt, String trangthai);
-	@Query("SELECT COUNT(d) FROM DangKyDetai d WHERE d.msdt.msgv.msgv = :msgv AND d.msdt.theLoai.matheloai = :matheloai AND d.trangthai = 'Đã duyệt'")
-	int countByGiangVienAndTheLoai(@Param("msgv") String msgv, @Param("matheloai") int matheloai);
-	List<DangKyDetai> findByMsdt_MsdtAndTrangthai(int msdt, String trangthai);
-	List<DangKyDetai> findByMsdt_Msgv_MsgvAndTrangthaiAndMsdt_TheLoai_Matheloai(String msgv, String trangthai, int matheloai);
-	
+    List<DangKyDetai> findByMsdt_Msdt(int msdt);
+    DangKyDetai findByMsdt_MsdtAndMssv_Mssv(int msdt, String mssv);
+    int countByMsdt_MsdtAndTrangthai(int msdt, String trangthai);
+
+    @Query("SELECT COUNT(d) FROM DangKyDetai d WHERE d.msdt.msgv.msgv = :msgv AND d.msdt.theLoai.matheloai = :matheloai AND d.trangthai = 'Đã duyệt'")
+    int countByGiangVienAndTheLoai(@Param("msgv") String msgv, @Param("matheloai") int matheloai);
+
+    List<DangKyDetai> findByMsdt_MsdtAndTrangthai(int msdt, String trangthai);
+    List<DangKyDetai> findByMsdt_Msgv_MsgvAndTrangthaiAndMsdt_TheLoai_Matheloai(String msgv, String trangthai, int matheloai);
 }
